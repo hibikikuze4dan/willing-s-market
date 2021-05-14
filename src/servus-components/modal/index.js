f2a61f90e0ffaafcab5ceaa0ad74883fe8b7f005
Hibiki Kuze <desu2dan@gmail.com>
2021-05-13 23:53:56 -0500

    update styles

--- src/servus-components/modal/index.js
+++ src/servus-components/modal/index.js
@@ -1,4 +1,4 @@
-import { Button, Dialog, Grid, TextField } from "@material-ui/core";
+import { Button, Dialog, Grid, makeStyles, TextField } from "@material-ui/core";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -11,16 +11,28 @@ import {
   updateServus,
 } from "../../redux/slice";
 
+const useStyles = makeStyles((theme) => ({
+  dialog: {
+    overflowY: "hidden",
+  },
+}));
+
 const ModalComponent = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const isOpen = useSelector(getDialogsByFunction)("servusi");
   const servus = useSelector(getCurrentServusFromServusi);
   const { name, tempImgSrc, id } = servus;
   return (
-    <Dialog open={isOpen} onClose={() => dispatch(toggleDialog("servusi"))}>
-      <Grid container>
+    <Dialog
+      classes={{ paper: classes.dialog }}
+      open={isOpen}
+      onClose={() => dispatch(toggleDialog("servusi"))}
+    >
+      <Grid container spacing={4} style={{ padding: "32px" }}>
         <Grid item xs={12}>
           <TextField
+            fullWidth
             label="Servus's Name"
             value={name}
             onChange={(e) =>
@@ -29,21 +41,25 @@ const ModalComponent = () => {
           />
         </Grid>
         <Grid item xs={12}>
-          <TextField
-            label="Image Src"
-            value={tempImgSrc}
-            onChange={(e) =>
-              dispatch(updateServus({ ...servus, tempImgSrc: e.target.value }))
-            }
-            helperText="Needs actual img link (ex. https://*.jpg)"
-          />
-          <Button
-            onClick={() => {
-              dispatch(saveServusTempLink(id));
-            }}
-          >
-            Save New Link
-          </Button>
+          <Grid container justify="space-between">
+            <TextField
+              label="Image Src"
+              value={tempImgSrc}
+              onChange={(e) =>
+                dispatch(
+                  updateServus({ ...servus, tempImgSrc: e.target.value })
+                )
+              }
+              helperText="Needs actual img link (ex. https://*.jpg)"
+            />
+            <Button
+              onClick={() => {
+                dispatch(saveServusTempLink(id));
+              }}
+            >
+              Save New Link
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     </Dialog>
